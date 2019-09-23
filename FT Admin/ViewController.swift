55e79cde088d99ffd6c16b78f5ee144901670a94
Tadreik <tadreik@gmail.com>
2019-09-23 05:41:57 -0400

    Initial Commit

---
+++ FT Admin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FT Admin
+//
+//  Created by Tadreik Campbell on 9/23/19.
+//  Copyright © 2019 Tadreik Campbell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
